dbfabf83e02c3de4032fb5c5fac6dd8190a72ea9
glebosaur <glebosaur@gmail.com>
2022-03-31 19:09:56 +0300

    commit message

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { WebsocketMockAdapter } from "./core/emmiter";
+import Messages from "./Messages"
 
 import "./App.css";
 
 function App({ websocket }) {
-  const [messages, setMessages] = useState([]);
+  const [state, setstate] = useState({
+    messages: [
+      {
+        text: "This is a test message!",
+        member: {
+          color: "blue",
+          username: "bluemoon"
+        }
+      }
+    ],
+    member: {
+      username: randomName(),
+      color: randomColor()
+    }
+  });
+  function randomName() {
+    const adjectives = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
+    const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
+    const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const noun = nouns[Math.floor(Math.random() * nouns.length)];
+    return adjective + noun;
+  }
+  
+  function randomColor() {
+    return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  }
 
-  useEffect(() => {
-    const websocket = new WebsocketMockAdapter((data) => {
-      messages.push(data);
-      setMessages(messages);
-    }, 3000);
-  }, []);
+  //const [messages, setMessages] = useState([]);
+
+  //useEffect(() => {
+   // const websocket = new WebsocketMockAdapter((data) => {
+    //  messages.push(data);
+    //  setMessages(messages);
+  //  }, 3000);
+  //}, []);
 
   const getUsers = () => console.log(websocket.getUsers());
 
@@ -19,6 +47,10 @@ function App({ websocket }) {
     <div className="App">
       <span>JS Junior Task</span>
       <button onClick={getUsers}>get users</button>
+      <Messages
+        messages={state.messages}
+        currentMember={state.member}
+      />
     </div>
   );
 }
